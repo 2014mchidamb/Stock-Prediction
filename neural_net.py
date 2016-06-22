8f4eea8fb430d3f7bb893d99b2a77101553cb6f8
Muthu Chidambaram <mutty@mutty.mtv.corp.google.com>
2016-06-21 17:36:28 -0700

    Started neural net code

--- neural_net.py
+++ neural_net.py
@@ -13,3 +13,26 @@ class NeuralNetwork(object):
     self.biases = [np.random.randn(y, 1) for y in self.sizes[1:]]
     self.weights = [np.random.randn(y, x)/np.sqrt(x)
                     for x, y in zip(self.sizes[:-1], self.sizes[1:])]
+
+  def feedforward(self, x):
+    for b, w in zip(self.biases, self.weights):
+      x = relu(np.dot(w, x)+b)
+    return x
+
+  def SGD(self, training_data, epochs, mini_batch_size, l_rate, lmda):
+    n = len(training_data)
+    for i in xrange(epochs):
+      random.shuffle(training_data)
+      mini_batches = [training_data[k:k+mini_batch_size]
+                      for k in xrange(0, n, mini_batch_size)]
+      for mini_batch in mini_batches:
+        self.update_mini_batch(mini_batch, l_rate, lmda, n)
+
+  def update_mini_batch(self, mini_batch, l_rate, lmbda, n):
+    nabla_b = [np.zeros(b.shape) for b in self.biases]
+    nabla_w = [np.zeros(w.shape) for w in self.weights]
+    for x, y in mini_batch:
+      delta_nabla_b, delta_nabla_w = self.backprop(x, y)
+      nabla_b = [nb+dnb for nb, dnb in zip(nabla_b, delta_nabla_b)]
+      nabla_w = [nw+dnw for nw, dnw in zip(nabla_w, delta_nabla_w)]
+    self.weights =
